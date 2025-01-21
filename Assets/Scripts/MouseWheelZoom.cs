69dc358fd963df1b5fce62285fc69d1ffed0d8ef
darrenwjoe <darrenwjoe@gmail.com>
2025-01-21 12:27:38 -0800

    MouseWheelZoom move camera towards cursor on zoom
    Can be configured

--- Assets/Scripts/MouseWheelZoom.cs
+++ Assets/Scripts/MouseWheelZoom.cs
@@ -4,6 +4,8 @@ public class MouseWheelZoom : MonoBehaviour
 {
     [SerializeField] private float scrollMultiplier = 0.1f;
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private bool zoomInTowardsCursor = true;
+    [SerializeField] private bool zoomOutTowardsCursor = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,15 +27,39 @@ public class MouseWheelZoom : MonoBehaviour
         if (mouseWheelInput == 0)
             return;
 
-        if (mouseWheelInput > 0)//Input.GetAxis(input.IsNewMouseScrollUp())                
+        Vector3 mouseWorldBeforeZoom = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        if (mouseWheelInput > 0) //Input.GetAxis(input.IsNewMouseScrollUp())
+        {
             MultiplyZoom(1 - scrollMultiplier);
-        else if (mouseWheelInput < 0)//(input.IsNewMouseScrollDown())
+            
+            if (zoomInTowardsCursor)
+                ShiftCameraTowardsCursor(false);
+        }
+        else if (mouseWheelInput < 0) //(input.IsNewMouseScrollDown())
+        {
             MultiplyZoom(1 + scrollMultiplier);
+            
+            if (zoomOutTowardsCursor)
+                ShiftCameraTowardsCursor(true);
+        }
         
         void MultiplyZoom(float multiplier)
         {
             if (mainCamera.orthographic)
                 mainCamera.orthographicSize *= multiplier;
+        }
+
+        void ShiftCameraTowardsCursor(bool invert = false)
+        {
+            Vector3 mouseWorldAfterZoom = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            var shift = mouseWorldBeforeZoom - mouseWorldAfterZoom;
+            
+            if (invert)
+                shift = -shift;
+            
+            // Adjust camera position to maintain focus on mouse cursor
+            mainCamera.transform.position += shift;
         }
     }
 }
