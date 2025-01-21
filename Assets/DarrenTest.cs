bab7c72b6a395f22c1307009bf27d79098de9054
darrenwjoe <darrenwjoe@gmail.com>
2025-01-20 22:20:37 -0800

    Test commit 2

--- Assets/DarrenTest.cs
+++ Assets/DarrenTest.cs
@@ -5,7 +5,7 @@ public class DarrenTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var greeting = "Hello World!";
     }
 
     // Update is called once per frame
