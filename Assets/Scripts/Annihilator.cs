279c4a8061680470b46aca415070a54fb84c5917
darrenwjoe <darrenwjoe@gmail.com>
2025-01-21 21:57:46 -0800

    Added failsafes against duplicate trigger to Annihilator

--- Assets/Scripts/Annihilator.cs
+++ Assets/Scripts/Annihilator.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Annihilator : MonoBehaviour
 {
     [SerializeField] private float shrinkTime = 1f;
+    [Header("Monitoring")]
+    [SerializeField] List<GameObject> shrinkingObjects = new List<GameObject>();
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +18,10 @@ public class Annihilator : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (shrinkingObjects.Contains(other.gameObject))
+            return;
+        
+        shrinkingObjects.Add(other.gameObject);
         StartCoroutine(Shrink(other.gameObject, shrinkTime));
     }
 
@@ -27,18 +34,16 @@ public class Annihilator : MonoBehaviour
 
         while (elapsedTime < time)
         {
+            if (!obj) // Someone else destroyed the object already.
+                yield break;
+                
             obj.transform.localScale = Vector3.Lerp(startScale, endScale, elapsedTime / time);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         transform.localScale = endScale; // Ensure exact target scale
+        shrinkingObjects.Remove(obj);
         Destroy(obj);    
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
