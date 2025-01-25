d78601c05022ece8f21e3ca18528ac2be8fdd3c2
Matthew Stevens <mstevens@llst.ca>
2025-01-24 19:56:02 -0800

    starting to stub in popper

--- Assets/Scripts/Obstacles/BubblePopper.cs
+++ Assets/Scripts/Obstacles/BubblePopper.cs
@@ -1,5 +1,15 @@
 
+using UnityEngine;
+
 public class BubblePopper
 {
-    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        var bubble = other.gameObject.GetComponent<Bubble>();
+        if (bubble)
+        {
+            bubble.PopBubble();
+        }
+
+    }
 }
