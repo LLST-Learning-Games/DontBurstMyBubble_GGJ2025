876fd8ff46de0a6e5fabd9f6f3543cbf91b823d7
RogueBrandon <brandon@rogueharbour.com>
2025-01-26 14:48:50 -0800

    Added a score calculation

--- Assets/Scripts/UI/ScoreLabel.cs
+++ Assets/Scripts/UI/ScoreLabel.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class ScoreLabel : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI Label;
+    public bool ShowCalcualtion;
 
     private void Update()
     {
-        Label.text = $"Score: {PlayerState.Current.Score:0}";
+        if (ShowCalcualtion)
+        {
+            Label.text = $"Score: {PlayerState.Current.Score:G}\n<size=80%>({PlayerState.Current.Score / 100:0} Bubbles x 100)";
+            return;
+        }
+        Label.text = $"Score: {PlayerState.Current.Score:G}";
     }
 }
