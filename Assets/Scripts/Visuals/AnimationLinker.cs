703eec78a0ddb7a0e69864de582eda17408b13b4
Matthew Stevens <mstevens@llst.ca>
2025-01-26 12:55:08 -0800

    fixing main menu

--- Assets/Scripts/Visuals/AnimationLinker.cs
+++ Assets/Scripts/Visuals/AnimationLinker.cs
@@ -5,9 +5,15 @@ using UnityEngine.Events;
 public class AnimationLinker : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onAnimationTrigger;
+    [SerializeField] private UnityEvent _onAnimationTriggerSecond;
 
     public void OnAnimationTrigger()
     {
         _onAnimationTrigger?.Invoke();
+    }
+    
+    public void OnAnimationTriggerSecond()
+    {
+        _onAnimationTriggerSecond?.Invoke();
     }
 }
