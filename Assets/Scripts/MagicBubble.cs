9810af21276f106234fefe380405b7db04dd94fd
darrenwjoe <darrenwjoe@gmail.com>
2025-01-25 19:59:53 -0800

    Revert "The attached bubbles can now collect the magic bubble"
    This reverts commit 126c24eaad3389902f56a226c2b8e50dc9235e7f.

--- Assets/Scripts/MagicBubble.cs
+++ Assets/Scripts/MagicBubble.cs
@@ -5,10 +5,11 @@ public class MagicBubble : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (!PhysicsUtility.IsPlayerOrAttachedTo(other.gameObject))
+        if (!other.gameObject.GetComponent<Player>())
             return;
 
         PlayerState.Current.Lives++;
         Destroy(gameObject);
+        //TODO: visibly parent the bubble to the player
     }
 }
